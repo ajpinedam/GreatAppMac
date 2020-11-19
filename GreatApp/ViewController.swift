f309daddf1b2cf567616e4eb08d35d62ea2ef0ea
Andres Pineda <pineda.andres@gmail.com>
2020-11-19 03:17:33 -0500

    Initial Commit

---
+++ GreatApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GreatApp
+//
+//  Created by Andres Pineda on 2020-11-19.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
